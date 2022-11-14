5603d3d337c0ae2c9b2c85d30eaf43b1714089fb
AswathiMohan23 <achu999here@gmail.com>
2022-11-14 07:27:24 +0530

    [ADDED] : code to remove a word from the given sentence

--- src/com/java/LinkedListMain.java
+++ src/com/java/LinkedListMain.java
@@ -4,20 +4,40 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
+//UC1 : Ability to find frequency of words in a sentence like “To be or not to be”
+//UC2 : Ability to find frequency of words in a large paragraph phrase “Paranoids are not paranoid because they are paranoid but
+        //because they keep putting themselves deliberately into paranoid avoidable situations”
+//UC3 : Remove avoidable word from the phrase “Paranoids are not paranoid because they are paranoid but because they keep putting themselves
+        //deliberately into paranoid avoidable situations”
+
 public class LinkedListMain {
+    static LinkedList<String> list=new LinkedList<>();
+    static LinkedList<String>listHashCode=new LinkedList<>();
+
     public static void main(String[] args)
     {
         findFrequency();
+        removingWords();
+    }
+
+    private static void removingWords() {
+        LinkedList<String> newList=new LinkedList<>();
+        newList=list;
+        String word="avoidable";
+        word=word.toLowerCase();
+        newList.remove(word);
+        System.out.println("\n============================================== UC3 ===========================================================" +
+                "\n\nthe remaining list is : "+newList);
     }
 
     public static void findFrequency() {
+        System.out.println("\n\n============================================== UC1 & UC2 ===================================================");
         long code=0;
         String sentence = "“Paranoids are not paranoid because they are paranoid but because they keep \n" +
                 "putting themselves deliberately into paranoid avoidable situations";
         sentence=sentence.toLowerCase();
        // char words=sentence.charAt()
-        LinkedList<String> list=new LinkedList<>();
-        LinkedList<String> secondList=new LinkedList<>();
+
 
         String array[] = sentence.split(" ");
         for(int i=0;i<array.length;i++){
@@ -31,11 +51,11 @@ public class LinkedListMain {
                 mapWords.put(i, 1);
             }
         }
-        System.out.println("frequency of words in he given sentence : "+mapWords);
+        System.out.println("\n\nfrequency of words in he given sentence : "+mapWords+"\n\n");
         for(int i=0;i<array.length;i++) {
             code = list.get(i).hashCode();
-            secondList.add(String.valueOf(code));
-            System.out.println("hash code of " + secondList.get(i) + " : " + secondList.get(i).hashCode());
+            listHashCode.add(String.valueOf(code));
+            System.out.println("hash code of " + listHashCode.get(i) + " : " + listHashCode.get(i).hashCode());
         }
     }
 
